9d3d583513d191d0d7fc2dd52e6dd25a398ad382
Ashrafuzzaman Remon <ashrafuzzaman@brainstation23.com>
2020-10-15 03:09:14 +0600

    Login Page Refactor

--- NewsPortal-Client/src/app/app.module.ts
+++ NewsPortal-Client/src/app/app.module.ts
@@ -6,7 +6,8 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { FooterComponent } from './Layout/footer/footer.component';
 import { SidebarComponent } from './Layout/sidebar/sidebar.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +17,9 @@ import { SidebarComponent } from './Layout/sidebar/sidebar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
